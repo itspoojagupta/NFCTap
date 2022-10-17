5d33e21e07c9dcfea072e65c805fb64e3281fe21
Pooja Kumari <pooja2310200@gmail.com>
2022-10-17 21:07:17 +0530

    Create site information

--- app/src/main/java/com/example/aircloudtap/SiteInformationActivity.kt
+++ app/src/main/java/com/example/aircloudtap/SiteInformationActivity.kt
@@ -3,12 +3,11 @@ package com.example.aircloudtap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class SiteInformationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        setContentView(R.layout.activity_forgot_password)
     }
 
 }
